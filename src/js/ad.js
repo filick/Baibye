8e7fd162dec19ad69e2506dd37becd19bf331058
filick <fuwangcosy@163.com>
2016-05-17 14:31:28 +0800

    add a block rule

---
+++ src/js/ad.js
@@ -0,0 +1,3 @@
+$("#head form#form.fm").attr("target", "_top");
+$("#container #page a").attr("target", "_top");
+$("#content_left>div").has("font:contains('- ')>a:contains('推广'), >a:contains('推广链接'), >a:contains('商业推广')").hide();
