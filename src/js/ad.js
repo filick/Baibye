083ee2808c12f81d9fa5a2cec7208bb52025cb8f
filick <fuwangcosy@163.com>
2016-09-17 09:14:56 +0800

    v1.2, add block rule for '广告'

--- src/js/ad.js
+++ src/js/ad.js
@@ -1,3 +1,3 @@
 $("#head form#form.fm").attr("target", "_top");
 $("#container #page a").attr("target", "_top");
-$("#content_left>div").has("font:contains('- ')>a:contains('推广'), >a:contains('推广链接'), >a:contains('商业推广')").hide();
+$("#content_left>div").has("font:contains('-')>a:contains('推广'), font:contains('-')>a:contains('广告'), >a:contains('推广链接'), >a:contains('商业推广')").hide();
